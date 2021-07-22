28468e4851b47917884ea72e317ebbb5bd783ffc
vepanimas <panimaskin94@gmail.com>
2021-07-22 21:12:47 +0300

    Enabled spellchecking for GraphQL (#477)

--- src/main/com/intellij/lang/jsgraphql/endpoint/JSGraphQLEndpointSpellcheckingStrategy.java
+++ src/main/com/intellij/lang/jsgraphql/endpoint/JSGraphQLEndpointSpellcheckingStrategy.java
@@ -7,13 +7,9 @@
  */
 package com.intellij.lang.jsgraphql.endpoint;
 
-import com.intellij.codeInspection.SuppressionUtil;
-import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNameIdentifierOwner;
-import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.spellchecker.inspections.IdentifierSplitter;
-import com.intellij.spellchecker.tokenizer.PsiIdentifierOwnerTokenizer;
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
 import com.intellij.spellchecker.tokenizer.Tokenizer;
 import com.intellij.spellchecker.tokenizer.TokenizerBase;
@@ -26,24 +22,12 @@ public class JSGraphQLEndpointSpellcheckingStrategy extends SpellcheckingStrateg
     @NotNull
     @Override
     public Tokenizer getTokenizer(PsiElement element) {
-        if (element instanceof PsiWhiteSpace) {
-            return EMPTY_TOKENIZER;
-        }
-        if (element instanceof PsiNameIdentifierOwner) {
-            return new PsiIdentifierOwnerTokenizer();
-        }
         if (element.getParent() instanceof PsiNameIdentifierOwner) {
             return EMPTY_TOKENIZER;
         }
         if (element.getNode().getElementType() == JSGraphQLEndpointTokenTypes.IDENTIFIER) {
             return IDENTIFIER_TOKENIZER;
         }
-        if (element instanceof PsiComment) {
-            if (SuppressionUtil.isSuppressionComment(element)) {
-                return EMPTY_TOKENIZER;
-            }
-            return myCommentTokenizer;
-        }
-        return EMPTY_TOKENIZER;
+        return super.getTokenizer(element);
     }
 }
---
+++ src/main/com/intellij/lang/jsgraphql/ide/spellchecking/GraphQLSpellcheckingStrategy.java
@@ -0,0 +1,17 @@
+package com.intellij.lang.jsgraphql.ide.spellchecking;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNameIdentifierOwner;
+import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
+import com.intellij.spellchecker.tokenizer.Tokenizer;
+import org.jetbrains.annotations.NotNull;
+
+public class GraphQLSpellcheckingStrategy extends SpellcheckingStrategy {
+    @Override
+    public @NotNull Tokenizer getTokenizer(PsiElement element) {
+        if (element.getParent() instanceof PsiNameIdentifierOwner) {
+            return EMPTY_TOKENIZER;
+        }
+        return super.getTokenizer(element);
+    }
+}
