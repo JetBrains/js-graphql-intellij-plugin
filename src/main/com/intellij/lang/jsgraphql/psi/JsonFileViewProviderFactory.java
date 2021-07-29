47df54ac34e9f9d9c256eff2cf6b1efdbeb2b9dc
vepanimas <panimaskin94@gmail.com>
2021-07-29 17:24:43 +0300

    Temporary workaround for huge JSON introspection schemas

---
+++ src/main/com/intellij/lang/jsgraphql/psi/JsonFileViewProviderFactory.java
@@ -0,0 +1,22 @@
+package com.intellij.lang.jsgraphql.psi;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.FileViewProviderFactory;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.SingleRootFileViewProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class JsonFileViewProviderFactory implements FileViewProviderFactory {
+    @Override
+    public @NotNull FileViewProvider createFileViewProvider(@NotNull VirtualFile file,
+                                                            Language language,
+                                                            @NotNull PsiManager manager,
+                                                            boolean eventSystemEnabled) {
+        if (!SingleRootFileViewProvider.isTooLargeForContentLoading(file)) {
+            SingleRootFileViewProvider.doNotCheckFileSizeLimit(file);
+        }
+        return new SingleRootFileViewProvider(manager, file, eventSystemEnabled);
+    }
+}
