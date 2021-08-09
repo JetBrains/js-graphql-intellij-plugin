3ff538ac994a227cb60578c778bc2c0f3a510d96
vepanimas <panimaskin94@gmail.com>
2021-08-09 17:06:22 +0300

    Replaced deprecated JSGraphQLEndpointHighlightAnnotator annotation API

--- src/main/com/intellij/lang/jsgraphql/endpoint/ide/annotator/JSGraphQLEndpointHighlightAnnotator.java
+++ src/main/com/intellij/lang/jsgraphql/endpoint/ide/annotator/JSGraphQLEndpointHighlightAnnotator.java
@@ -7,32 +7,30 @@
  */
 package com.intellij.lang.jsgraphql.endpoint.ide.annotator;
 
-import com.intellij.lang.jsgraphql.endpoint.ide.highlighting.JSGraphQLEndpointSyntaxHighlighter;
-import org.jetbrains.annotations.NotNull;
-
-import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.lang.jsgraphql.endpoint.ide.highlighting.JSGraphQLEndpointSyntaxHighlighter;
 import com.intellij.lang.jsgraphql.endpoint.psi.JSGraphQLEndpointNamedType;
 import com.intellij.lang.jsgraphql.endpoint.psi.JSGraphQLEndpointNamedTypeDef;
 import com.intellij.lang.jsgraphql.endpoint.psi.JSGraphQLEndpointProperty;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
 
 public class JSGraphQLEndpointHighlightAnnotator implements Annotator {
-	@Override
-	public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-		if(element instanceof JSGraphQLEndpointNamedTypeDef) {
-			setTextAttributes(element, holder, JSGraphQLEndpointSyntaxHighlighter.DEF);
-		} else if (element instanceof JSGraphQLEndpointProperty) {
-			setTextAttributes(element, holder, JSGraphQLEndpointSyntaxHighlighter.PROPERTY);
-		} else if (element instanceof JSGraphQLEndpointNamedType) {
-			setTextAttributes(element, holder, JSGraphQLEndpointSyntaxHighlighter.ATOM);
-		}
-	}
+    @Override
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        if (element instanceof JSGraphQLEndpointNamedTypeDef) {
+            setTextAttributes(element, holder, JSGraphQLEndpointSyntaxHighlighter.DEF);
+        } else if (element instanceof JSGraphQLEndpointProperty) {
+            setTextAttributes(element, holder, JSGraphQLEndpointSyntaxHighlighter.PROPERTY);
+        } else if (element instanceof JSGraphQLEndpointNamedType) {
+            setTextAttributes(element, holder, JSGraphQLEndpointSyntaxHighlighter.ATOM);
+        }
+    }
 
-	private void setTextAttributes(PsiElement element, AnnotationHolder holder, TextAttributesKey key) {
-		final Annotation annotation = holder.createInfoAnnotation(element, null);
-		annotation.setTextAttributes(key);
-	}
+    private void setTextAttributes(PsiElement element, AnnotationHolder holder, TextAttributesKey key) {
+        holder.newSilentAnnotation(HighlightSeverity.INFORMATION).range(element).textAttributes(key).create();
+    }
 }
