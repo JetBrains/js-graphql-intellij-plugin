57e3208d00a992665480401dc1e053e801dab900
vepanimas <panimaskin94@gmail.com>
2021-11-15 21:15:36 +0300

    Don't complete selected lookup element on a dot char

---
+++ src/main/com/intellij/lang/jsgraphql/ide/completion/GraphQLCharFilter.java
@@ -0,0 +1,24 @@
+package com.intellij.lang.jsgraphql.ide.completion;
+
+import com.intellij.codeInsight.lookup.CharFilter;
+import com.intellij.codeInsight.lookup.Lookup;
+import com.intellij.lang.jsgraphql.psi.GraphQLFile;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.Nullable;
+
+public class GraphQLCharFilter extends CharFilter {
+    @Override
+    public @Nullable Result acceptChar(char c, int prefixLength, Lookup lookup) {
+        PsiFile file = lookup.getPsiFile();
+        if (!(file instanceof GraphQLFile)) {
+            return null;
+        }
+
+        // not to complete an object field from shown popup when a fragment spread is typed
+        if (c == '.') {
+            return Result.HIDE_LOOKUP;
+        }
+
+        return null;
+    }
+}
