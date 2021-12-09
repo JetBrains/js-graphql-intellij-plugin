4ba6717d776f140cded499f8c3253d71287d83cc
vepanimas <panimaskin94@gmail.com>
2021-12-10 01:06:19 +0300

    Cleanup SentryClient API

--- src/main/com/intellij/lang/jsgraphql/ide/diagnostic/GraphQLSentryClient.java
+++ src/main/com/intellij/lang/jsgraphql/ide/diagnostic/GraphQLSentryClient.java
@@ -4,7 +4,7 @@ import io.sentry.Sentry;
 import io.sentry.SentryEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class GraphQLSentryClient {
+public final class GraphQLSentryClient {
     private static class Lazy {
         private static final GraphQLSentryClient ourClient = new GraphQLSentryClient();
     }
@@ -13,7 +13,7 @@ public class GraphQLSentryClient {
         return Lazy.ourClient;
     }
 
-    public GraphQLSentryClient() {
+    private GraphQLSentryClient() {
         Sentry.init(options -> options.setDsn("https://56294687de4a4eda977a0e2be35a785c@o1086110.ingest.sentry.io/6097822"));
     }
 
