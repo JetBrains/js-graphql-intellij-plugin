4d0542e4157cd2d0a1efa2f2f264a0d076f128e8
vepanimas <panimaskin94@gmail.com>
2023-02-05 21:33:51 +0200

    extract config name constants

--- src/main/com/intellij/lang/jsgraphql/ide/config/GraphQLConfigConstants.kt
+++ src/main/com/intellij/lang/jsgraphql/ide/config/GraphQLConfigConstants.kt
@@ -2,12 +2,12 @@
 
 package com.intellij.lang.jsgraphql.ide.config
 
+const val GRAPHQLCONFIG_COMMENT = ".graphqlconfig="
+
 const val GRAPHQLCONFIG = ".graphqlconfig"
 const val GRAPHQLCONFIG_JSON = ".graphqlconfig.json"
 const val GRAPHQLCONFIG_YML = ".graphqlconfig.yml"
 const val GRAPHQLCONFIG_YAML = ".graphqlconfig.yaml"
-
-const val GRAPHQLCONFIG_COMMENT = ".graphqlconfig="
 
 val LEGACY_CONFIG_NAMES: Set<String> = linkedSetOf(
     GRAPHQLCONFIG,
@@ -16,24 +16,29 @@ val LEGACY_CONFIG_NAMES: Set<String> = linkedSetOf(
     GRAPHQLCONFIG_YAML,
 )
 
+const val GRAPHQL_CONFIG_JSON = "graphql.config.json"
+const val GRAPHQL_CONFIG_YAML = "graphql.config.yaml"
 const val GRAPHQL_CONFIG_YML = "graphql.config.yml"
 const val GRAPHQL_RC = ".graphqlrc"
+const val GRAPHQL_RC_JSON = ".graphqlrc.json"
+const val GRAPHQL_RC_YML = ".graphqlrc.yml"
+const val GRAPHQL_RC_YAML = ".graphqlrc.yaml"
 
 val MODERN_CONFIG_NAMES: Set<String> = linkedSetOf(
 //    "graphql.config.ts",
 //    "graphql.config.js",
 //    "graphql.config.cjs",
-    "graphql.config.json",
-    "graphql.config.yaml",
+    GRAPHQL_CONFIG_JSON,
+    GRAPHQL_CONFIG_YAML,
     GRAPHQL_CONFIG_YML,
 //    "graphql.config.toml",
     GRAPHQL_RC,
 //    ".graphqlrc.ts",
 //    ".graphqlrc.js",
 //    ".graphqlrc.cjs",
-    ".graphqlrc.json",
-    ".graphqlrc.yml",
-    ".graphqlrc.yaml",
+    GRAPHQL_RC_JSON,
+    GRAPHQL_RC_YML,
+    GRAPHQL_RC_YAML,
 //    ".graphqlrc.toml",
 //    "package.json",
 )
