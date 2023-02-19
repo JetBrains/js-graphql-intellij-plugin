5877b903fe32556307b84e2a706005f85f8acff8
vepanimas <panimaskin94@gmail.com>
2023-02-19 05:58:40 +0200

    JSON config introspection

--- src/main/com/intellij/lang/jsgraphql/ide/introspection/editor/json/GraphQLIntrospectionJsonLineMarkerProvider.kt
+++ src/main/com/intellij/lang/jsgraphql/ide/introspection/editor/json/GraphQLIntrospectionJsonLineMarkerProvider.kt
@@ -5,214 +5,70 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package com.intellij.lang.jsgraphql.ide.introspection.editor.json;
+package com.intellij.lang.jsgraphql.ide.introspection.editor.json
 
-import com.google.gson.Gson;
-import com.intellij.codeInsight.daemon.LineMarkerInfo;
-import com.intellij.codeInsight.daemon.LineMarkerProvider;
-import com.intellij.icons.AllIcons;
-import com.intellij.json.psi.JsonObject;
-import com.intellij.json.psi.JsonProperty;
-import com.intellij.json.psi.JsonStringLiteral;
-import com.intellij.json.psi.JsonValue;
-import com.intellij.lang.jsgraphql.GraphQLBundle;
-import com.intellij.lang.jsgraphql.ide.config.GraphQLConfigConstants;
-import com.intellij.lang.jsgraphql.ide.config.env.GraphQLConfigEnvironmentParser;
-import com.intellij.lang.jsgraphql.ide.config.loader.GraphQLRawEndpoint;
-import com.intellij.lang.jsgraphql.ide.config.model.GraphQLConfigEndpoint;
-import com.intellij.lang.jsgraphql.ide.introspection.GraphQLIntrospectionService;
-import com.intellij.lang.jsgraphql.ide.notifications.GraphQLNotificationUtil;
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
-import com.intellij.openapi.editor.markup.GutterIconRenderer;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiErrorElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.util.PsiUtilCore;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.LineMarkerProvider
+import com.intellij.json.psi.*
+import com.intellij.lang.jsgraphql.ide.config.CONFIG_NAMES
+import com.intellij.lang.jsgraphql.ide.config.loader.GraphQLConfigKeys
+import com.intellij.lang.jsgraphql.ide.introspection.createIntrospectionLineMarker
+import com.intellij.psi.PsiElement
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Stream;
+class GraphQLIntrospectionJsonLineMarkerProvider : LineMarkerProvider {
+    override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
+        val property = element as? JsonProperty ?: return null
+        val file = element.containingFile as? JsonFile ?: return null
+        val virtualFile = file.virtualFile ?: return null
+        if (virtualFile.name !in CONFIG_NAMES || virtualFile.name == "package.json") return null
 
-/**
- * Line marker for running an introspection against a configured endpoint url in a .graphqlconfig file
- */
-public class GraphQLIntrospectionJsonLineMarkerProvider implements LineMarkerProvider {
-    private static final Boolean IS_LEGACY = true; // TODO: should depend on the file
+        val endpointsProperty = getParentPropertyWithName(property, GraphQLConfigKeys.EXTENSION_ENDPOINTS) ?: return null
+        val extensionsProperty = getParentPropertyWithName(endpointsProperty, GraphQLConfigKeys.EXTENSIONS) ?: return null
 
-    @Nullable
-    @Override
-    public LineMarkerInfo<?> getLineMarkerInfo(@NotNull PsiElement element) {
-        // TODO: support more filenames
-        if (!GraphQLConfigConstants.GRAPHQLCONFIG.equals(element.getContainingFile().getName())) {
-            return null;
+        val isRoot = isRootProperty(extensionsProperty)
+        val projectName = if (!isRoot) {
+            getParentProperty(extensionsProperty)?.name
+        } else {
+            null
         }
-        if (!(element instanceof JsonProperty)) {
-            return null;
+        if (!isRoot && projectName.isNullOrBlank()) {
+            return null
         }
-        final JsonProperty jsonProperty = (JsonProperty) element;
-        final Ref<String> urlRef = Ref.create();
-        if (isEndpointUrl(jsonProperty, urlRef) && !hasErrors(jsonProperty.getContainingFile())) {
-            PsiElement anchor = jsonProperty.getNameElement().getFirstChild();
-            if (anchor == null) return null;
 
-            return new LineMarkerInfo<>(
-                anchor,
-                anchor.getTextRange(),
-                AllIcons.RunConfigurations.TestState.Run,
-                o -> GraphQLBundle.message("graphql.introspection.run.query"),
-                (evt, el) -> {
-                    String introspectionUrl;
-                    if (jsonProperty.getValue() instanceof JsonStringLiteral) {
-                        introspectionUrl = ((JsonStringLiteral) jsonProperty.getValue()).getValue();
-                    } else {
-                        return;
-                    }
+        val isValidEndpoint = when (val value = property.value) {
+            is JsonStringLiteral -> true
 
-                    final GraphQLConfigEndpoint endpoint = getEndpoint(introspectionUrl, jsonProperty);
-                    if (endpoint == null) {
-                        return;
-                    }
+            is JsonObject -> value
+                .findProperty(GraphQLConfigKeys.EXTENSION_ENDPOINT_URL)
+                ?.takeIf { it.value is JsonStringLiteral } != null
 
-                    String schemaPath = getSchemaPath(jsonProperty, true);
-                    if (StringUtil.isEmptyOrSpaces(schemaPath)) {
-                        return;
-                    }
-
-                    final Project project = element.getProject();
-
-                    GraphQLIntrospectionService.getInstance(project).performIntrospectionQueryAndUpdateSchemaPathFile(endpoint, schemaPath);
-                },
-                GutterIconRenderer.Alignment.CENTER,
-                () -> GraphQLBundle.message("graphql.introspection.run.query")
-            );
+            else -> false
         }
-        return null;
-    }
 
-    private boolean hasErrors(PsiFile file) {
-        return PsiTreeUtil.findChildOfType(file, PsiErrorElement.class) != null;
-    }
-
-    private boolean isEndpointUrl(JsonProperty jsonProperty, Ref<String> urlRef) {
-        if (jsonProperty.getValue() instanceof JsonStringLiteral) {
-            final String url = ((JsonStringLiteral) jsonProperty.getValue()).getValue();
-            if (isUrlOrVariable(jsonProperty.getProject(), url)) {
-                final JsonProperty parentProperty = PsiTreeUtil.getParentOfType(jsonProperty, JsonProperty.class);
-                final JsonProperty grandParentProperty = PsiTreeUtil.getParentOfType(parentProperty, JsonProperty.class);
-                if ("url".equals(jsonProperty.getName()) &&
-                    grandParentProperty != null &&
-                    "endpoints".equals(grandParentProperty.getName())) {
-                    // "endpoints": {
-                    //      "<name>": {
-                    //          "url": "url" <---
-                    //      }
-                    // }
-                    urlRef.set(url);
-                    return true;
-                }
-                if (parentProperty != null && "endpoints".equals(parentProperty.getName())) {
-                    // "endpoints": {
-                    //      "<name>": "url" <---
-                    // }
-                    urlRef.set(url);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private boolean isUrlOrVariable(@NotNull Project project, @NotNull String jsonValue) {
-        try {
-            new URL(jsonValue);
-            return true;
-        } catch (MalformedURLException e) {
-            return GraphQLConfigEnvironmentParser.getInstance(project).containsVariables(jsonValue, IS_LEGACY);
+        return if (isValidEndpoint)
+            createIntrospectionLineMarker(property.project, property.name, virtualFile, projectName, property)
+        else {
+            null
         }
     }
 
-    @Nullable
-    private String getSchemaPath(@NotNull JsonProperty urlElement, boolean showNotificationOnMissingPath) {
-        JsonObject jsonObject = PsiTreeUtil.getParentOfType(urlElement, JsonObject.class);
-        while (jsonObject != null) {
-            JsonProperty schemaPathElement = jsonObject.findProperty("schemaPath");
-            if (schemaPathElement != null) {
-                if (schemaPathElement.getValue() instanceof JsonStringLiteral) {
-                    String schemaPath = ((JsonStringLiteral) schemaPathElement.getValue()).getValue();
-                    if (StringUtil.isEmptyOrSpaces(schemaPath)) {
-                        GraphQLNotificationUtil.showInvalidConfigurationNotification(
-                            GraphQLBundle.message("graphql.notification.empty.schema.path"),
-                            null,
-                            urlElement.getProject()
-                        );
-                    }
-                    return schemaPath;
-                } else {
-                    break;
-                }
-            }
-            jsonObject = PsiTreeUtil.getParentOfType(jsonObject, JsonObject.class);
-        }
-        if (showNotificationOnMissingPath) {
-            GraphQLNotificationUtil.showInvalidConfigurationNotification(
-                GraphQLBundle.message("graphql.notification.empty.schema.path"),
-                null,
-                urlElement.getProject()
-            );
-        }
-        return null;
+    private fun getParentPropertyWithName(element: JsonElement?, name: String): JsonProperty? {
+        return getParentProperty(element)?.takeIf { it.name == name }
     }
 
-    private @Nullable GraphQLConfigEndpoint getEndpoint(@NotNull String url, @NotNull JsonProperty urlJsonProperty) {
-        try {
-            VirtualFile virtualFile = PsiUtilCore.getVirtualFile(urlJsonProperty);
-            VirtualFile dir = virtualFile != null ? virtualFile.getParent() : null;
-            if (dir == null) {
-                return null;
-            }
-
-            // if the endpoint is just the url string, headers are not supported
-            final JsonProperty parent = PsiTreeUtil.getParentOfType(urlJsonProperty, JsonProperty.class);
-            final boolean supportsHeaders = parent != null && !"endpoints".equals(parent.getName());
-
-            final String name = supportsHeaders ? parent.getName() : url;
-
-            final GraphQLRawEndpoint endpointConfig = new GraphQLRawEndpoint(name, url, Collections.emptyMap(), false);
-
-            if (supportsHeaders) {
-                final Stream<JsonProperty> jsonPropertyStream = PsiTreeUtil.getChildrenOfTypeAsList(urlJsonProperty.getParent(),
-                    JsonProperty.class).stream();
-                final Optional<JsonProperty> headers = jsonPropertyStream.filter(p -> "headers".equals(p.getName())).findFirst();
-                headers.ifPresent(headersProp -> {
-                    final JsonValue jsonValue = headersProp.getValue();
-                    if (jsonValue != null) {
-                        endpointConfig.setHeaders(new Gson().<Map<String, Object>>fromJson(jsonValue.getText(), Map.class));
-                    }
-                });
-            }
-
-            return new GraphQLConfigEndpoint(urlJsonProperty.getProject(), endpointConfig, dir, null, IS_LEGACY, true);
-
-        } catch (Exception e) {
-            Notifications.Bus.notify(new Notification(
-                GraphQLNotificationUtil.GRAPHQL_NOTIFICATION_GROUP_ID,
-                GraphQLBundle.message("graphql.notification.configuration.error"),
-                e.getMessage(),
-                NotificationType.ERROR
-            ), urlJsonProperty.getProject());
+    private fun getParentProperty(element: JsonElement?): JsonProperty? {
+        var parent = element?.parent
+        if (parent is JsonObject) {
+            parent = parent.parent
         }
-        return null;
+        return parent as? JsonProperty
+    }
+
+    private fun isRootProperty(element: JsonElement?): Boolean {
+        return element?.parent
+            ?.takeIf { it is JsonObject }
+            ?.parent
+            ?.let { it is JsonFile }
+            ?: false
     }
 }
