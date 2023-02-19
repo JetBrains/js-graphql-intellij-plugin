890c3ce3d920d0c98b6d491e3c423a143865e7bf
vepanimas <panimaskin94@gmail.com>
2023-02-19 04:47:35 +0200

    JSON schema for configs including IJ Community versions

---
+++ src/main/com/intellij/lang/jsgraphql/ide/config/jsonSchema/GraphQLConfigJsonSchemaProvider.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2018-present, Jim Kynde Meyer
+ * All rights reserved.
+ * <p>
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package com.intellij.lang.jsgraphql.ide.config.jsonSchema
+
+import com.intellij.lang.jsgraphql.GraphQLBundle
+import com.intellij.lang.jsgraphql.ide.config.*
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
+import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
+import com.jetbrains.jsonSchema.extension.SchemaType
+
+
+class GraphQLConfigJsonSchemaProvider : JsonSchemaProviderFactory {
+    companion object {
+        private val MODERN_YML_JSON_CONFIGS = arrayOf(
+            GRAPHQL_CONFIG_JSON,
+            GRAPHQL_CONFIG_YAML,
+            GRAPHQL_CONFIG_YML,
+            GRAPHQL_RC,
+            GRAPHQL_RC_JSON,
+            GRAPHQL_RC_YAML,
+            GRAPHQL_RC_YML,
+        )
+    }
+
+    override fun getProviders(project: Project): List<JsonSchemaFileProvider> {
+        return listOf<JsonSchemaFileProvider>(
+            GraphQLEmbeddedJsonSchemaFileProvider(
+                "graphql-config-schema.json",
+                GraphQLBundle.message("graphql.config.legacy.schema.name"),
+                null,
+                GraphQLConfigJsonSchemaProvider::class.java,
+                "/schemas/",
+                GRAPHQLCONFIG,
+            ),
+            GraphQLEmbeddedJsonSchemaFileProvider(
+                "graphql-config.json",
+                GraphQLBundle.message("graphql.config.schema.name"),
+                null,
+                GraphQLConfigJsonSchemaProvider::class.java,
+                "/schemas/",
+                *MODERN_YML_JSON_CONFIGS,
+            )
+        )
+    }
+
+    private class GraphQLEmbeddedJsonSchemaFileProvider(
+        private val resourceName: String,
+        private val presentableName: String?,
+        private val remoteSourceUrl: String?,
+        clazz: Class<*>,
+        pathToFilename: String,
+        vararg files: String,
+    ) : JsonSchemaFileProvider {
+
+        private val schemaFile: VirtualFile?
+        private val userFilename: Set<String>
+
+        init {
+            userFilename = if (files.isEmpty()) setOf(resourceName) else files.toSet()
+            schemaFile = JsonSchemaProviderFactory.getResourceFile(clazz, pathToFilename + resourceName)
+        }
+
+        override fun isAvailable(file: VirtualFile): Boolean {
+            return userFilename.contains(file.name)
+        }
+
+        override fun getName(): String {
+            return resourceName
+        }
+
+        override fun getSchemaFile(): VirtualFile? {
+            return schemaFile
+        }
+
+        override fun getSchemaType(): SchemaType {
+            return SchemaType.embeddedSchema
+        }
+
+        override fun getPresentableName(): String {
+            return presentableName ?: name
+        }
+
+        override fun isUserVisible(): Boolean {
+            return presentableName != null
+        }
+
+        override fun getRemoteSource(): String? {
+            return remoteSourceUrl
+        }
+    }
+
+}
