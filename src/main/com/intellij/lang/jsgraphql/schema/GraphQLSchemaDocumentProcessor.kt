3c75903e6f6b9d00e3f3a9d6bb6e1878a7db109b
vepanimas <panimaskin94@gmail.com>
2023-02-25 16:13:16 +0200

    Create a default Query type if no explicit schema definitions exist

--- src/main/com/intellij/lang/jsgraphql/schema/GraphQLSchemaDocumentProcessor.kt
+++ src/main/com/intellij/lang/jsgraphql/schema/GraphQLSchemaDocumentProcessor.kt
@@ -1,34 +1,23 @@
-package com.intellij.lang.jsgraphql.schema;
+package com.intellij.lang.jsgraphql.schema
 
-import com.intellij.lang.jsgraphql.psi.GraphQLFile;
-import com.intellij.lang.jsgraphql.schema.builder.GraphQLCompositeRegistry;
-import com.intellij.lang.jsgraphql.types.language.Document;
-import com.intellij.psi.PsiFile;
-import com.intellij.util.Processor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.lang.jsgraphql.psi.GraphQLFile
+import com.intellij.lang.jsgraphql.schema.builder.GraphQLCompositeRegistry
+import com.intellij.psi.PsiFile
+import com.intellij.util.Processor
 
-public class GraphQLSchemaDocumentProcessor implements Processor<PsiFile> {
-    private final GraphQLCompositeRegistry myCompositeRegistry = new GraphQLCompositeRegistry();
-    private boolean isProcessedGraphQL;
+class GraphQLSchemaDocumentProcessor : Processor<PsiFile?> {
+    val compositeRegistry = GraphQLCompositeRegistry()
 
-    public @NotNull GraphQLCompositeRegistry getCompositeRegistry() {
-        return myCompositeRegistry;
-    }
+    var isProcessed = false
+        private set
 
-    public boolean isProcessed() {
-        return isProcessedGraphQL;
-    }
-
-    @Override
-    public boolean process(@Nullable PsiFile psiFile) {
-        if (!(psiFile instanceof GraphQLFile)) {
-            return true;
+    override fun process(psiFile: PsiFile?): Boolean {
+        if (psiFile !is GraphQLFile) {
+            return true
         }
-
-        isProcessedGraphQL = true;
-        Document document = ((GraphQLFile) psiFile).getDocument();
-        myCompositeRegistry.addFromDocument(document);
-        return true;
+        isProcessed = true
+        val document = psiFile.document
+        compositeRegistry.addFromDocument(document)
+        return true
     }
 }
