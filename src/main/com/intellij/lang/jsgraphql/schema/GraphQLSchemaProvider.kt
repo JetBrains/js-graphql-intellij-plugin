c05719d5e3b0ecbc5a5b4f2df1e3c3691135c9fc
vepanimas <panimaskin94@gmail.com>
2023-02-27 12:00:32 +0200

    Backward compatibility with DGS plugin

--- src/main/com/intellij/lang/jsgraphql/schema/GraphQLSchemaProvider.kt
+++ src/main/com/intellij/lang/jsgraphql/schema/GraphQLSchemaProvider.kt
@@ -27,6 +27,7 @@ import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import com.intellij.util.concurrency.annotations.RequiresReadLock
 import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval
 import java.util.concurrent.ConcurrentMap
 
 @Service(Service.Level.PROJECT)
@@ -86,5 +87,12 @@ class GraphQLSchemaProvider(project: Project) {
                 )
             }
         }
+    }
+
+    @Deprecated("use GraphQLRegistryProvider.getRegistryInfo() instead")
+    @ScheduledForRemoval
+    @RequiresReadLock
+    fun getRegistryInfo(context: PsiElement?): GraphQLRegistryInfo {
+        return registryProvider.getRegistryInfo(context)
     }
 }
