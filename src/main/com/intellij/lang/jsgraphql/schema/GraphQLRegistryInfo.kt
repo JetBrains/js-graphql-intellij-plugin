06a180842c67102271da163d514a3693862da833
vepanimas <panimaskin94@gmail.com>
2023-02-27 12:29:19 +0200

    Remove registryInfo.hasProcessedAnyFile() as not needed since we process at least the Specification file

--- src/main/com/intellij/lang/jsgraphql/schema/GraphQLRegistryInfo.kt
+++ src/main/com/intellij/lang/jsgraphql/schema/GraphQLRegistryInfo.kt
@@ -5,41 +5,20 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package com.intellij.lang.jsgraphql.schema;
+package com.intellij.lang.jsgraphql.schema
 
-import com.intellij.lang.jsgraphql.types.GraphQLException;
-import com.intellij.lang.jsgraphql.types.schema.idl.TypeDefinitionRegistry;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.lang.jsgraphql.types.GraphQLException
+import com.intellij.lang.jsgraphql.types.schema.idl.TypeDefinitionRegistry
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class GraphQLRegistryInfo {
-
-    private final TypeDefinitionRegistry myRegistry;
-    private final List<GraphQLException> myErrors;
-    private final boolean myHasProcessedAnyFile;
-
-    public GraphQLRegistryInfo(@NotNull TypeDefinitionRegistry registry,
-                               @NotNull List<GraphQLException> errors,
-                               boolean processedAnyFile) {
-        myRegistry = registry;
-        myErrors = errors;
-        myHasProcessedAnyFile = processedAnyFile;
-    }
-
-    public @NotNull TypeDefinitionRegistry getTypeDefinitionRegistry() {
-        return myRegistry;
-    }
-
-    public @NotNull List<GraphQLException> getErrors() {
-        List<GraphQLException> errors = new ArrayList<>();
-        errors.addAll(myRegistry.getErrors());
-        errors.addAll(myErrors);
-        return errors;
-    }
-
-    public boolean hasProcessedAnyFile() {
-        return myHasProcessedAnyFile;
-    }
+class GraphQLRegistryInfo(
+    val typeDefinitionRegistry: TypeDefinitionRegistry,
+    private val additionalErrors: List<GraphQLException>,
+) {
+    val errors: List<GraphQLException>
+        get() {
+            val errors = mutableListOf<GraphQLException>()
+            errors.addAll(typeDefinitionRegistry.errors)
+            errors.addAll(additionalErrors)
+            return errors
+        }
 }
--- src/main/com/intellij/lang/jsgraphql/schema/GraphQLSchemaInfo.kt
+++ src/main/com/intellij/lang/jsgraphql/schema/GraphQLSchemaInfo.kt
@@ -5,63 +5,41 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package com.intellij.lang.jsgraphql.schema;
+package com.intellij.lang.jsgraphql.schema
 
-import com.google.common.collect.Lists;
-import com.intellij.lang.jsgraphql.ide.validation.GraphQLErrorFilter;
-import com.intellij.lang.jsgraphql.types.GraphQLError;
-import com.intellij.lang.jsgraphql.types.GraphQLException;
-import com.intellij.lang.jsgraphql.types.schema.GraphQLSchema;
-import com.intellij.lang.jsgraphql.types.schema.idl.errors.SchemaProblem;
-import com.intellij.lang.jsgraphql.types.schema.validation.InvalidSchemaException;
-import com.intellij.openapi.project.Project;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
+import com.google.common.collect.Lists
+import com.intellij.lang.jsgraphql.ide.validation.GraphQLErrorFilter
+import com.intellij.lang.jsgraphql.types.GraphQLError
+import com.intellij.lang.jsgraphql.types.GraphQLException
+import com.intellij.lang.jsgraphql.types.schema.GraphQLSchema
+import com.intellij.lang.jsgraphql.types.schema.idl.errors.SchemaProblem
+import com.intellij.lang.jsgraphql.types.schema.validation.InvalidSchemaException
+import com.intellij.openapi.project.Project
 
-import java.util.List;
+class GraphQLSchemaInfo(
+    val schema: GraphQLSchema,
+    private val additionalErrors: List<GraphQLException>,
+    val registryInfo: GraphQLRegistryInfo,
+) {
+    fun getErrors(project: Project): List<GraphQLError> {
+        val rawErrors: MutableList<GraphQLException> = Lists.newArrayList(additionalErrors)
+        rawErrors.addAll(registryInfo.errors)
+        rawErrors.addAll(schema.errors)
 
-public class GraphQLSchemaInfo {
-
-    private final GraphQLSchema mySchema;
-    private final List<GraphQLException> myErrors;
-    private final GraphQLRegistryInfo myRegistry;
-
-    public GraphQLSchemaInfo(@NotNull GraphQLSchema schema,
-                             @NotNull List<GraphQLException> errors,
-                             @NotNull GraphQLRegistryInfo registry) {
-        mySchema = schema;
-        myErrors = errors;
-        myRegistry = registry;
-    }
-
-    public @NotNull GraphQLSchema getSchema() {
-        return mySchema;
-    }
-
-    public @NotNull GraphQLRegistryInfo getRegistryInfo() {
-        return myRegistry;
-    }
-
-    public @NotNull List<GraphQLError> getErrors(@NotNull Project project) {
-        final List<GraphQLException> rawErrors = Lists.newArrayList(myErrors);
-        rawErrors.addAll(myRegistry.getErrors());
-        rawErrors.addAll(mySchema.getErrors());
-
-        final List<GraphQLError> errors = Lists.newArrayList();
-        for (GraphQLException exception : rawErrors) {
-            if (exception instanceof SchemaProblem) {
-                errors.addAll(((SchemaProblem) exception).getErrors());
-            } else if (exception instanceof GraphQLError) {
-                errors.add((GraphQLError) exception);
-            } else if (exception instanceof InvalidSchemaException) {
-                errors.addAll(((InvalidSchemaException) exception).getErrors());
-            } else {
-                errors.add(new GraphQLUnexpectedSchemaError(exception));
+        val errors = mutableListOf<GraphQLError>()
+        for (exception in rawErrors) {
+            when (exception) {
+                is SchemaProblem -> errors.addAll(exception.errors)
+                is GraphQLError -> errors.add(exception as GraphQLError)
+                is InvalidSchemaException -> errors.addAll(exception.errors)
+                else -> errors.add(GraphQLUnexpectedSchemaError(exception))
             }
         }
 
-        return ContainerUtil.filter(errors, error ->
-            GraphQLErrorFilter.EP_NAME.extensions().noneMatch(filter -> filter.isGraphQLErrorSuppressed(project, error, null))
-        );
+        return errors.filter { error: GraphQLError ->
+            GraphQLErrorFilter.EP_NAME.extensionList.none { filter: GraphQLErrorFilter ->
+                filter.isGraphQLErrorSuppressed(project, error, null)
+            }
+        }
     }
 }
