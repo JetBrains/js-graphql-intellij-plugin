d3b0f081dd90bc6c943303e547b459bd333cf7a9
vepanimas <panimaskin94@gmail.com>
2023-04-24 17:12:28 +0300

    Fix processDirectoriesUpToContentRoot for Rider #659

--- src/main/com/intellij/lang/jsgraphql/ide/resolve/GraphQLResolveUtil.java
+++ src/main/com/intellij/lang/jsgraphql/ide/resolve/GraphQLResolveUtil.java
@@ -5,8 +5,8 @@ import com.intellij.lang.jsgraphql.schema.library.GraphQLLibrary;
 import com.intellij.lang.jsgraphql.schema.library.GraphQLLibraryDescriptor;
 import com.intellij.lang.jsgraphql.schema.library.GraphQLLibraryRootsProvider;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.BaseProjectDirectories;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -108,22 +108,17 @@ public final class GraphQLResolveUtil {
         if (project.isDisposed()) {
             return;
         }
-        ProjectFileIndex fileIndex = ProjectFileIndex.getInstance(project);
         VirtualFile dir = fileOrDir.isDirectory() ? fileOrDir : fileOrDir.getParent();
         if (dir == null) {
             return;
         }
-        VirtualFile contentRoot = fileIndex.getContentRootForFile(dir, false);
-        while (dir != null && contentRoot != null) {
+        VirtualFile stopAt = BaseProjectDirectories.getInstance(project).getBaseDirectoryFor(dir);
+        while (dir != null && stopAt != null) {
             if (!directoryProcessor.process(dir)) {
                 return;
             }
-            if (dir.equals(contentRoot)) {
-                dir = dir.getParent();
-                if (dir == null) {
-                    return;
-                }
-                contentRoot = fileIndex.getContentRootForFile(dir, false);
+            if (dir.equals(stopAt)) {
+                return;
             } else {
                 dir = dir.getParent();
             }
