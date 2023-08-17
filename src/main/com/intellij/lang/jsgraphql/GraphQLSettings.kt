efb22faebeec2956c4d33c8d426e7f44e3e7c072
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2023-08-17 20:09:59 +0300

    [graphql] convert GraphQLSettings to kt
    GitOrigin-RevId: 3783f33988fee63183da2b906d2edf2a5240bac7

--- src/main/com/intellij/lang/jsgraphql/GraphQLSettings.kt
+++ src/main/com/intellij/lang/jsgraphql/GraphQLSettings.kt
@@ -5,130 +5,90 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package com.intellij.lang.jsgraphql;
+package com.intellij.lang.jsgraphql
 
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.ModificationTracker;
-import com.intellij.openapi.util.SimpleModificationTracker;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.lang.jsgraphql.GraphQLSettings.GraphQLSettingsState
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.project.Project
 
 /**
  * Project-wide GraphQL settings persisted in the .idea folder as graphql-settings.xml
  */
-@State(name = "GraphQLSettings", storages = {@Storage("graphql-settings.xml")})
-public class GraphQLSettings implements PersistentStateComponent<GraphQLSettings.GraphQLSettingsState> {
+@Service(Service.Level.PROJECT)
+@State(name = "GraphQLSettings", storages = [Storage("graphql-settings.xml")])
+class GraphQLSettings : PersistentStateComponent<GraphQLSettingsState> {
 
-  private final SimpleModificationTracker myModificationTracker = new SimpleModificationTracker();
-  private GraphQLSettingsState myState = new GraphQLSettingsState();
+  private var state = GraphQLSettingsState()
 
-  public static GraphQLSettings getSettings(@NotNull Project project) {
-    return project.getService(GraphQLSettings.class);
-  }
+  override fun getState(): GraphQLSettingsState = state
 
-  @Nullable
-  @Override
-  public GraphQLSettings.GraphQLSettingsState getState() {
-    return myState;
-  }
-
-  @Override
-  public void loadState(@NotNull GraphQLSettings.GraphQLSettingsState state) {
-    myState = state;
-    settingsChanged();
-  }
-
-  public ModificationTracker getModificationTracker() {
-    return myModificationTracker;
-  }
-
-  private void settingsChanged() {
-    myModificationTracker.incModificationCount();
+  override fun loadState(newState: GraphQLSettingsState) {
+    state = newState
   }
 
   /* Introspection */
 
-  public String getIntrospectionQuery() {
-    return myState.introspectionQuery;
-  }
+  var introspectionQuery: String
+    get() = state.introspectionQuery
+    set(introspectionQuery) {
+      state.introspectionQuery = introspectionQuery
+    }
 
-  public void setIntrospectionQuery(String introspectionQuery) {
-    myState.introspectionQuery = introspectionQuery;
-    settingsChanged();
-  }
+  var isEnableIntrospectionDefaultValues: Boolean
+    get() = state.enableIntrospectionDefaultValues
+    set(enableIntrospectionDefaultValues) {
+      state.enableIntrospectionDefaultValues = enableIntrospectionDefaultValues
+    }
 
-  public boolean isEnableIntrospectionDefaultValues() {
-    return myState.enableIntrospectionDefaultValues;
-  }
+  var isEnableIntrospectionRepeatableDirectives: Boolean
+    get() = state.enableIntrospectionRepeatableDirectives
+    set(enableIntrospectionRepeatableDirectives) {
+      state.enableIntrospectionRepeatableDirectives = enableIntrospectionRepeatableDirectives
+    }
 
-  public void setEnableIntrospectionDefaultValues(boolean enableIntrospectionDefaultValues) {
-    myState.enableIntrospectionDefaultValues = enableIntrospectionDefaultValues;
-    settingsChanged();
-  }
-
-  public boolean isEnableIntrospectionRepeatableDirectives() {
-    return myState.enableIntrospectionRepeatableDirectives;
-  }
-
-  public void setEnableIntrospectionRepeatableDirectives(boolean enableIntrospectionRepeatableDirectives) {
-    myState.enableIntrospectionRepeatableDirectives = enableIntrospectionRepeatableDirectives;
-    settingsChanged();
-  }
-
-  public boolean isOpenEditorWithIntrospectionResult() {
-    return myState.openEditorWithIntrospectionResult;
-  }
-
-  public void setOpenEditorWithIntrospectionResult(boolean openEditorWithIntrospectionResult) {
-    myState.openEditorWithIntrospectionResult = openEditorWithIntrospectionResult;
-    settingsChanged();
-  }
+  var isOpenEditorWithIntrospectionResult: Boolean
+    get() = state.openEditorWithIntrospectionResult
+    set(openEditorWithIntrospectionResult) {
+      state.openEditorWithIntrospectionResult = openEditorWithIntrospectionResult
+    }
 
   /* Frameworks */
 
-  public boolean isRelaySupportEnabled() {
-    return myState.enableRelayModernFrameworkSupport;
+  var isRelaySupportEnabled: Boolean
+    get() = state.enableRelayModernFrameworkSupport
+    set(enableRelayModernFrameworkSupport) {
+      state.enableRelayModernFrameworkSupport = enableRelayModernFrameworkSupport
+    }
+
+  var isFederationSupportEnabled: Boolean
+    get() = state.enableFederationSupport
+    set(enableFederationSupport) {
+      state.enableFederationSupport = enableFederationSupport
+    }
+
+  var isApolloKotlinSupportEnabled: Boolean
+    get() = state.enableApolloKotlinSupport
+    set(enableApolloKotlinSupport) {
+      state.enableApolloKotlinSupport = enableApolloKotlinSupport
+    }
+
+  class GraphQLSettingsState {
+    var introspectionQuery = ""
+    var enableIntrospectionDefaultValues = true
+    var enableIntrospectionRepeatableDirectives = false
+    var openEditorWithIntrospectionResult = true
+    var enableRelayModernFrameworkSupport = false
+    var enableFederationSupport = false
+    var enableApolloKotlinSupport = false
   }
 
-  public void setRelaySupportEnabled(boolean enableRelayModernFrameworkSupport) {
-    myState.enableRelayModernFrameworkSupport = enableRelayModernFrameworkSupport;
-    settingsChanged();
-  }
-
-  public boolean isFederationSupportEnabled() {
-    return myState.enableFederationSupport;
-  }
-
-  public void setFederationSupportEnabled(boolean enableFederationSupport) {
-    myState.enableFederationSupport = enableFederationSupport;
-    settingsChanged();
-  }
-
-  public boolean isApolloKotlinSupportEnabled() {
-    return myState.enableApolloKotlinSupport;
-  }
-
-  public void setApolloKotlinSupportEnabled(boolean enableApolloKotlinSupport) {
-    myState.enableApolloKotlinSupport = enableApolloKotlinSupport;
-    settingsChanged();
-  }
-
-  /**
-   * The state class that is persisted as XML
-   * NOTE!!!: 1. Class must be static, and 2. Fields must be public for settings serialization to work
-   */
-  public static class GraphQLSettingsState {
-    public String introspectionQuery = "";
-    public boolean enableIntrospectionDefaultValues = true;
-    public boolean enableIntrospectionRepeatableDirectives = false;
-    public boolean openEditorWithIntrospectionResult = true;
-
-    public boolean enableRelayModernFrameworkSupport;
-    public boolean enableFederationSupport = false;
-    public boolean enableApolloKotlinSupport = false;
+  companion object {
+    @JvmStatic
+    fun getSettings(project: Project): GraphQLSettings {
+      return project.getService(GraphQLSettings::class.java)
+    }
   }
 }
-
