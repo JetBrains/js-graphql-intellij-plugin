470398a242f2cbd9fe0249fb866d432629827f97
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2024-06-15 13:38:25 +0200

    IJPL-150231 Move icon mappings from json to icon generator
    GitOrigin-RevId: 4d4576fc7eeb92ec43c36abec2d62d271b45d9a4

--- gen/com/intellij/lang/jsgraphql/GraphqlIcons.java
+++ gen/com/intellij/lang/jsgraphql/GraphqlIcons.java
@@ -10,30 +10,30 @@ import javax.swing.*;
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
 public final class GraphqlIcons {
-  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
-    return IconManager.getInstance().loadRasterizedIcon(path, GraphqlIcons.class.getClassLoader(), cacheKey, flags);
+  private static @NotNull Icon load(@NotNull String path, @NotNull String expUIPath, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, expUIPath, GraphqlIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Apollo = load("icons/apollo.svg", -919539896, 2);
-  /** 16x16 */ public static final @NotNull Icon Attribute = load("icons/attribute.svg", -769791984, 0);
-  /** 16x16 */ public static final @NotNull Icon Enum = load("icons/enum.svg", 1141726372, 1);
-  /** 16x16 */ public static final @NotNull Icon Field = load("icons/field.svg", 902226195, 0);
-  /** 16x16 */ public static final @NotNull Icon Fragment = load("icons/fragment.svg", -987352060, 0);
-  /** 16x16 */ public static final @NotNull Icon Graphql = load("icons/graphql.svg", -566102188, 2);
-  /** 16x16 */ public static final @NotNull Icon GraphqlConfig = load("icons/graphqlConfig.svg", -1342426823, 2);
-  /** 16x16 */ public static final @NotNull Icon GraphqlNode = load("icons/graphqlNode.svg", -342439323, 2);
-  /** 16x16 */ public static final @NotNull Icon GraphqlSchema = load("icons/graphqlSchema.svg", -1229132693, 2);
-  /** 13x13 */ public static final @NotNull Icon GraphqlToolWindow = load("icons/graphqlToolWindow.svg", -1239891742, 2);
+  /** 16x16 */ public static final @NotNull Icon Apollo = load("icons/apollo.svg", "icons/newui/apollo.svg", -919539896, 2);
+  /** 16x16 */ public static final @NotNull Icon Attribute = load("icons/attribute.svg", "icons/newui/attribute.svg", -769791984, 0);
+  /** 16x16 */ public static final @NotNull Icon Enum = load("icons/enum.svg", "icons/newui/enum.svg", 1141726372, 1);
+  /** 16x16 */ public static final @NotNull Icon Field = load("icons/field.svg", "icons/newui/field.svg", 902226195, 0);
+  /** 16x16 */ public static final @NotNull Icon Fragment = load("icons/fragment.svg", "icons/newui/fragment.svg", -987352060, 0);
+  /** 16x16 */ public static final @NotNull Icon Graphql = load("icons/graphql.svg", "icons/newui/graphql.svg", -566102188, 2);
+  /** 16x16 */ public static final @NotNull Icon GraphqlConfig = load("icons/graphqlConfig.svg", "icons/newui/graphqlConfig.svg", -1342426823, 2);
+  /** 16x16 */ public static final @NotNull Icon GraphqlNode = load("icons/graphqlNode.svg", "icons/newui/graphqlNode.svg", -342439323, 2);
+  /** 16x16 */ public static final @NotNull Icon GraphqlSchema = load("icons/graphqlSchema.svg", "icons/newui/graphqlSchema.svg", -1229132693, 2);
+  /** 13x13 */ public static final @NotNull Icon GraphqlToolWindow = load("icons/graphqlToolWindow.svg", "icons/newui/graphqlToolWindow.svg", -1239891742, 2);
 
   public static final class Gutter {
-    /** 12x12 */ public static final @NotNull Icon GraphQL = load("icons/gutter/graphQL.svg", -214176870, 2);
+    /** 12x12 */ public static final @NotNull Icon GraphQL = load("icons/gutter/graphQL.svg", "icons/newui/graphql@14x14.svg", -214176870, 2);
   }
 
-  /** 16x16 */ public static final @NotNull Icon Interface = load("icons/interface.svg", 1051857627, 1);
-  /** 16x16 */ public static final @NotNull Icon Mutation = load("icons/mutation.svg", -753752934, 1);
-  /** 16x16 */ public static final @NotNull Icon Query = load("icons/query.svg", -648140378, 0);
-  /** 16x16 */ public static final @NotNull Icon Relay = load("icons/relay.svg", 1540049466, 0);
-  /** 16x16 */ public static final @NotNull Icon Scalar = load("icons/scalar.svg", -1239951075, 0);
-  /** 16x16 */ public static final @NotNull Icon Subscription = load("icons/subscription.svg", -448188111, 0);
-  /** 16x16 */ public static final @NotNull Icon Type = load("icons/type.svg", -1533426216, 0);
-  /** 16x16 */ public static final @NotNull Icon Variable = load("icons/variable.svg", -1232158411, 0);
+  /** 16x16 */ public static final @NotNull Icon Interface = load("icons/interface.svg", "icons/newui/interface.svg", 1051857627, 1);
+  /** 16x16 */ public static final @NotNull Icon Mutation = load("icons/mutation.svg", "icons/newui/mutation.svg", -753752934, 1);
+  /** 16x16 */ public static final @NotNull Icon Query = load("icons/query.svg", "icons/newui/query.svg", -648140378, 0);
+  /** 16x16 */ public static final @NotNull Icon Relay = load("icons/relay.svg", "icons/newui/relay.svg", 1540049466, 0);
+  /** 16x16 */ public static final @NotNull Icon Scalar = load("icons/scalar.svg", "icons/newui/scalar.svg", -1239951075, 0);
+  /** 16x16 */ public static final @NotNull Icon Subscription = load("icons/subscription.svg", "icons/newui/subscription.svg", -448188111, 0);
+  /** 16x16 */ public static final @NotNull Icon Type = load("icons/type.svg", "icons/newui/type.svg", -1533426216, 0);
+  /** 16x16 */ public static final @NotNull Icon Variable = load("icons/variable.svg", "icons/newui/variable.svg", -1232158411, 0);
 }
