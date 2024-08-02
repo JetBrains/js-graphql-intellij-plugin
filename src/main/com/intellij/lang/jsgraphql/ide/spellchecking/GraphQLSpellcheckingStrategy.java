d1722d7f602e8894cad0ccf9d38c0b87de48dbe4
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-08-02 22:48:21 +0200

    IJPL-155874 Make most of the spellchecking strategies dumb aware
    If you see any problems with your spellchecking strategy, please feel free to remove DumbAware from it. Or, you can make it dumb-aware by avoiding using indexes in dumb mode
    
    The only strategies that are not DumbAware at the moment are:
    - JavaSpellcheckingStrategy
    - JSSpellCheckingStrategy
    
    GitOrigin-RevId: 74781cf1ebd9b58669396bf7b1c03a839019f12b

--- src/main/com/intellij/lang/jsgraphql/ide/spellchecking/GraphQLSpellcheckingStrategy.java
+++ src/main/com/intellij/lang/jsgraphql/ide/spellchecking/GraphQLSpellcheckingStrategy.java
@@ -1,13 +1,14 @@
 package com.intellij.lang.jsgraphql.ide.spellchecking;
 
 import com.intellij.lang.jsgraphql.psi.GraphQLStringLiteral;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNameIdentifierOwner;
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
 import com.intellij.spellchecker.tokenizer.Tokenizer;
 import org.jetbrains.annotations.NotNull;
 
-public final class GraphQLSpellcheckingStrategy extends SpellcheckingStrategy {
+public final class GraphQLSpellcheckingStrategy extends SpellcheckingStrategy implements DumbAware {
   @Override
   public @NotNull Tokenizer getTokenizer(PsiElement element) {
     if (element.getParent() instanceof PsiNameIdentifierOwner) {
