3745166e05bce88e09795dc94d30ba0e6a91d8ac
Max Medvedev <maxim.medvedev@jetbrains.com>
2024-08-09 10:14:41 +0200

    [graphql] Don't create new instances of HighlightDisplayKey in the wild
    It does not make sense to pass a new instance of HDK to the inspection profile, it does not know anything about it anyway
    
    GitOrigin-RevId: 172817dcf7490766dc91bdc051fec5d81f66e6db

--- src/main/com/intellij/lang/jsgraphql/ide/validation/inspections/GraphQLInspection.java
+++ src/main/com/intellij/lang/jsgraphql/ide/validation/inspections/GraphQLInspection.java
@@ -9,7 +9,6 @@ import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInspection.*;
 import com.intellij.codeInspection.ex.InspectionProfileImpl;
 import com.intellij.codeInspection.ex.LocalInspectionToolWrapper;
-import com.intellij.codeInspection.ex.ToolsImpl;
 import com.intellij.lang.annotation.AnnotationBuilder;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.HighlightSeverity;
@@ -17,6 +16,7 @@ import com.intellij.lang.annotation.ProblemGroup;
 import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.lang.jsgraphql.ide.validation.GraphQLProblemGroup;
 import com.intellij.lang.jsgraphql.schema.library.GraphQLLibraryManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.profile.codeInspection.InspectionProjectProfileManager;
@@ -31,39 +31,52 @@ import java.util.List;
 import java.util.function.Function;
 
 public abstract class GraphQLInspection extends LocalInspectionTool {
+  private static final Logger LOG = Logger.getInstance(GraphQLInspection.class);
+
   public static boolean isToolEnabled(@NotNull Project project,
                                       @NotNull Class<? extends GraphQLInspection> inspectionClass,
                                       @Nullable PsiFile file) {
-    InspectionProfileImpl inspectionProfile = InspectionProjectProfileManager.getInstance(project).getCurrentProfile();
-    HighlightDisplayKey key = getHighlightDisplayKeyByClass(inspectionClass);
-    ToolsImpl tools = inspectionProfile.getToolsOrNull(key.toString(), project);
-    return tools != null && tools.isEnabled(file);
+    HighlightDisplayKey highlightDisplayKey = getHighlightDisplayKeyByClass(inspectionClass);
+    if (highlightDisplayKey == null) {
+      return false;
+    }
+    return isToolEnabled(project, highlightDisplayKey, file);
   }
 
+  private static boolean isToolEnabled(@NotNull Project project,
+                                       @NotNull HighlightDisplayKey highlightDisplayKey,
+                                       @Nullable PsiFile file) {
+    InspectionProfileImpl inspectionProfile = InspectionProjectProfileManager.getInstance(project).getCurrentProfile();
+    return inspectionProfile.isToolEnabled(highlightDisplayKey, file);
+  }
 
-  public static @NotNull HighlightDisplayKey getHighlightDisplayKeyByClass(@NotNull Class<? extends GraphQLInspection> inspectionClass) {
-    String shortName = InspectionProfileEntry.getShortName(inspectionClass.getSimpleName());
-    HighlightDisplayKey key = HighlightDisplayKey.find(shortName);
-    if (key == null) {
-      key = new HighlightDisplayKey(shortName, shortName);
-    }
-    return key;
+  private static @Nullable HighlightDisplayKey getHighlightDisplayKeyByClass(@NotNull Class<? extends GraphQLInspection> inspectionClass) {
+    String shortName = getShortName(inspectionClass.getSimpleName());
+    return HighlightDisplayKey.find(shortName);
   }
 
   public static @NotNull String getInspectionShortName(@NotNull Class<? extends GraphQLInspection> inspectionClass) {
-    return getHighlightDisplayKeyByClass(inspectionClass).toString();
+    return getShortName(inspectionClass.getSimpleName());
   }
 
-  public static @NotNull HighlightSeverity getSeverity(@NotNull Class<? extends GraphQLInspection> inspectionClass,
-                                                       @NotNull PsiElement element) {
-
-    return getHighlightDisplayLevel(inspectionClass, element).getSeverity();
+  private static @NotNull HighlightSeverity getSeverity(@NotNull HighlightDisplayKey highlightDisplayKey,
+                                                        @NotNull PsiElement element) {
+    return getHighlightDisplayLevel(highlightDisplayKey, element).getSeverity();
   }
 
   public static @NotNull HighlightDisplayLevel getHighlightDisplayLevel(@NotNull Class<? extends GraphQLInspection> inspectionClass,
                                                                         @NotNull PsiElement element) {
-    InspectionProfile inspectionProfile = InspectionProjectProfileManager.getInstance(element.getProject()).getCurrentProfile();
     HighlightDisplayKey highlightDisplayKey = getHighlightDisplayKeyByClass(inspectionClass);
+    if (highlightDisplayKey == null) {
+      LOG.error("Inspection " + inspectionClass + " is not registered");
+      return HighlightDisplayLevel.ERROR;
+    }
+    return getHighlightDisplayLevel(highlightDisplayKey, element);
+  }
+
+  private static @NotNull HighlightDisplayLevel getHighlightDisplayLevel(@NotNull HighlightDisplayKey highlightDisplayKey,
+                                                                         @NotNull PsiElement element) {
+    InspectionProfile inspectionProfile = InspectionProjectProfileManager.getInstance(element.getProject()).getCurrentProfile();
     return inspectionProfile.getErrorLevel(highlightDisplayKey, element);
   }
 
@@ -131,13 +144,16 @@ public abstract class GraphQLInspection extends LocalInspectionTool {
 
     if (inspectionClass != null) {
       HighlightDisplayKey key = getHighlightDisplayKeyByClass(inspectionClass);
-      String toolId = key.toString();
+      if (key == null) {
+        return null;
+      }
+      String toolId = key.getShortName();
       PsiFile file = element.getContainingFile();
       Project project = element.getProject();
 
       InspectionProfileImpl profile = InspectionProjectProfileManager.getInstance(project).getCurrentProfile();
       LocalInspectionToolWrapper inspectionToolWrapper = getInspectionToolWrapper(element, toolId, profile);
-      if (inspectionToolWrapper == null || !isToolEnabled(project, inspectionClass, file)) {
+      if (inspectionToolWrapper == null || !isToolEnabled(project, key, file)) {
         return null;
       }
 
@@ -146,7 +162,7 @@ public abstract class GraphQLInspection extends LocalInspectionTool {
         return null;
       }
 
-      severity = getSeverity(inspectionClass, element.getContainingFile());
+      severity = getSeverity(key, element.getContainingFile());
       problemGroup = new GraphQLProblemGroup(toolId);
 
       String displayName = HighlightDisplayKey.getDisplayNameByKey(key);
