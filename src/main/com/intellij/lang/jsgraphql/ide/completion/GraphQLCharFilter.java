35dcd38754a83240239aaee425c60317b9759dc0
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-12-22 19:46:22 +0100

    cleanup [completion]: nullability for CharFilters
    GitOrigin-RevId: f0338967098d9d1e1c52ff147e9c9cddf729c9e3

--- src/main/com/intellij/lang/jsgraphql/ide/completion/GraphQLCharFilter.java
+++ src/main/com/intellij/lang/jsgraphql/ide/completion/GraphQLCharFilter.java
@@ -4,11 +4,12 @@ import com.intellij.codeInsight.lookup.CharFilter;
 import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.lang.jsgraphql.psi.GraphQLFile;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public final class GraphQLCharFilter extends CharFilter {
   @Override
-  public @Nullable Result acceptChar(char c, int prefixLength, Lookup lookup) {
+  public @Nullable Result acceptChar(char c, int prefixLength, @NotNull Lookup lookup) {
     PsiFile file = lookup.getPsiFile();
     if (!(file instanceof GraphQLFile)) {
       return null;
