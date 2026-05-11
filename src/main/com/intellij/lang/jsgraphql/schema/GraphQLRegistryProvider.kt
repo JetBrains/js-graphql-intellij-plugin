844bbeedc8a3cbf98da2cb35702255f5dc94d214
Nikolay Chashnikov <nikolay.chashnikov@jetbrains.com>
2026-05-11 14:55:36 +0200

    IJPL-234888 js graphql: remove unused deprecated API
    GitOrigin-RevId: f99c0d7776910ce1e196c6a77b55a081f5ec1b45

--- src/main/com/intellij/lang/jsgraphql/schema/GraphQLRegistryProvider.kt
+++ src/main/com/intellij/lang/jsgraphql/schema/GraphQLRegistryProvider.kt
@@ -12,7 +12,6 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.annotations.ApiStatus
 
 @Deprecated("Use GraphQLSchemaProvider instead")
@@ -28,14 +27,10 @@ class GraphQLRegistryProvider(private val project: Project) {
   @Deprecated("Use GraphQLSchemaProvider instead")
   @ApiStatus.ScheduledForRemoval
   fun getRegistryInfo(context: PsiElement?): GraphQLRegistryInfo {
-    return getRegistryInfo(GraphQLScopeProvider.getInstance(project).getResolveScope(context, true))
-  }
-
-  @Deprecated("Use GraphQLSchemaProvider instead")
-  @ApiStatus.ScheduledForRemoval
-  fun getRegistryInfo(schemaScope: GlobalSearchScope): GraphQLRegistryInfo {
+    val schemaScope = GraphQLScopeProvider.getInstance(project).getResolveScope(context, true)
     val schemaInfo = GraphQLSchemaProvider.getInstance(project).getSchemaInfo(schemaScope)
     return GraphQLRegistryInfo(schemaInfo.registry, schemaInfo.isTooComplex)
   }
+
 }
 
